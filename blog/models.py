4966114681e08886d0c16c4622b1c661f47b3258
RobWar-code <rowaniar@aol.com>
2023-03-25 20:30:49 +0000

    migrate updates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+# Added to provide a user model
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, "draft"), (1, "published"))
+
+
+# For the model layout - refer to Projects/Webcourse/DjangoProject.txt
+#  - Designing the Database
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # The CASCADE setting indicates that when the entry is deleted the effect
+    # cascades to other tables
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
+
+    # Additional features
+    class Meta:
+        # The - indicates descending order
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.body} by {self.name}"
