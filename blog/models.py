d1e0e7cba47e7d78617ae5ff899fa1faeee4be94
RobWar-code <rowaniar@aol.com>
2023-03-26 10:32:03 +0000

    Add admin post form and amend content with summernote

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     # The CASCADE setting indicates that when the entry is deleted the effect
     # cascades to other tables
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -36,7 +37,8 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
