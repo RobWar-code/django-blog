f7ab769fa31080622c66b62718afa30b87475155
RobWar-code <rowaniar@aol.com>
2023-03-31 09:12:16 +0000

    Add alert messaging

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -62,6 +63,14 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
